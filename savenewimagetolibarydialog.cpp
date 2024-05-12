3c3a335f640aaf7ad00637455d72bc540bac1df3
Steopik <kamushev.05@gmail.com>
2024-05-12 19:52:47 +0300

    3Commit

---
+++ savenewimagetolibarydialog.cpp
@@ -0,0 +1,3 @@
+#include "savenewimagetolibarydialog.h"
+
+SaveNewImageToLibaryDialog::SaveNewImageToLibaryDialog() {}
---
+++ savenewimagetolibarydialog.h
@@ -0,0 +1,15 @@
+#ifndef SAVENEWIMAGETOLIBARYDIALOG_H
+#define SAVENEWIMAGETOLIBARYDIALOG_H
+
+#include <QDialog>
+#include <QObject>
+#include <QPushButton>
+#include <QLabel>
+
+class SaveNewImageToLibaryDialog: public QDialog
+{
+public:
+    SaveNewImageToLibaryDialog();
+};
+
+#endif // SAVENEWIMAGETOLIBARYDIALOG_H
